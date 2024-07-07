da623c239792f0adbcd07dc33b88491573f7c40d
evcgit <evanvcortez@gmail.com>
2024-07-06 22:17:37 -0600

    tailwind and environment bug fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
 	const [data, setData] = React.useState(null);
@@ -12,12 +10,14 @@ function App() {
 	}, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+		<div className='flex justify-center flex-col content-center items-center h-screen bg-gray-600'>
+			<h1> {data ? data : "Pending backend startup"} </h1>
+    	<form className='flex flex-col'>
+				<input type="text" name="username" placeholder='Username'className='border-2 border-black rounded mb-2 p-1'/>
+				<input type="password" name="password" placeholder='Password'className='border-2 border-black rounded mb-2 p-1'/>
+				<input type="submit" value="Submit" className='bg-slate-500 hover:bg-slate-700 text-white py-2 px-4 rounded'/>
+			</form>
+		</div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api', (req, res) => {
-	res.json({ message: 'Hello from server!' });
+	res.json({ message: 'Backend Online!' });
 });
 
 app.listen(PORT, () => {
