1e621c2ee076bacb93e0295d3b0a85744208f6c3
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-07-11 08:58:53 -0600

    profile page

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -13,6 +13,10 @@ const Profile = () => {
         username: 'johndoe',
         password: 'password',
     });
+    const [firstName, setFirstName] = useState(user.firstName);
+    const [lastName, setLastName] = useState(user.lastName);
+    const [email, setEmail] = useState(user.email);
+    const [phone, setPhone] = useState(user.phone);
 
     const [editMode, setEditMode] = useState(false);
     const [updatedUser, setUpdatedUser] = useState(user);
@@ -31,46 +35,79 @@ const Profile = () => {
     };
 
     return (
-        <div className='bg-gray-900 h-screen flex flex-col'>
+        <div className='bg-gray-900 h-screen w-screen flex flex-col'>
             <Header />
             <div className='flex justify-center items-center mt-10 h-full'>
                 <div className='w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 bg-white rounded-lg shadow-lg p-8'>
                     <h1 className='text-3xl font-bold text-gray-800 mb-6 text-center'>Profile</h1>
                     {!editMode ? (
                         <div className='space-y-4'>
-                            <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-                                <div className='text-gray-700'>
-                                    <p className='text-lg font-semibold'><strong>First Name:</strong></p>
-                                    <p className='text-md'>{user.firstName}</p>
+                            <div className="flex flex-col w-full">
+                                <div className='text-center'>
+                                    <input
+                                        type="text"
+                                        value={firstName}
+                                        onChange={(e) => setFirstName(e.target.value)}
+                                        name="firstName"
+                                        placeholder="First Name"
+                                        className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none mx-1"
+                                        disabled='true'
+                                    />
+                                    <input
+                                        type="text"
+                                        value={lastName}
+                                        onChange={(e) => setLastName(e.target.value)}
+                                        name="lastName"
+                                        placeholder="Last Name"
+                                        className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none mx-1"
+                                        disabled='true'
+                                    />
+                                    <input
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                        name="email"
+                                        placeholder="Email"
+                                        className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none mx-1"
+                                        disabled='true'
+                                    />
+                                    <input
+                                        type="tel"
+                                        value={phone}
+                                        onChange={(e) => setPhone(e.target.value)}
+                                        name="phone"
+                                        placeholder="Phone Number"
+                                        className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none mx-1"
+                                        disabled='true'
+                                    />
+                                    <input
+                                        type="text"
+                                        value={user.username}
+                                        name="username"
+                                        placeholder="Username"
+                                        className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none mx-1"
+                                        disabled='true'
+                                    />
+                                    <input
+                                        type="password"
+                                        value={user.password}
+                                        name="password"
+                                        placeholder="Password"
+                                        className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none mx-1"
+                                        disabled='true'
+                                    />
+                                    <div />
+                                    <button
+                                        className='mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow hover:bg-blue-600 transition duration-300'
+                                        onClick={() => setEditMode(true)}
+                                    >
+                                        Edit
+                                    </button>
                                 </div>
-                                <div className='text-gray-700'>
-                                    <p className='text-lg font-semibold'><strong>Last Name:</strong></p>
-                                    <p className='text-md'>{user.lastName}</p>
-                                </div>
-                                <div className='text-gray-700'>
-                                    <p className='text-lg font-semibold'><strong>Email:</strong></p>
-                                    <p className='text-md'>{user.email}</p>
-                                </div>
-                                <div className='text-gray-700'>
-                                    <p className='text-lg font-semibold'><strong>Phone:</strong></p>
-                                    <p className='text-md'>{user.phone}</p>
-                                </div>
-                                <div className='text-gray-700'>
-                                    <p className='text-lg font-semibold'><strong>Username:</strong></p>
-                                    <p className='text-md'>{user.username}</p>
-                                </div>
-                            </div>
-                            <div className='text-center'>
-                                <button 
-                                    className='mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow hover:bg-blue-600 transition duration-300'
-                                    onClick={() => setEditMode(true)}
-                                >
-                                    Edit
-                                </button>
                             </div>
                         </div>
                     ) : (
-                        <form className="flex flex-wrap space-y-4">
+                        <form className="flex flex-wrap">
                             <div className="flex flex-col w-full md:w-1/2 pr-2">
                                 <input
                                     type="text"
@@ -78,14 +115,6 @@ const Profile = () => {
                                     onChange={handleChange}
                                     name="firstName"
                                     placeholder="First Name"
-                                    className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
-                                />
-                                <input
-                                    type="text"
-                                    value={updatedUser.lastName}
-                                    onChange={handleChange}
-                                    name="lastName"
-                                    placeholder="Last Name"
                                     className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
                                 />
                                 <input
@@ -97,29 +126,11 @@ const Profile = () => {
                                     className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
                                 />
                                 <input
-                                    type="tel"
-                                    value={updatedUser.phone}
-                                    onChange={handleChange}
-                                    name="phone"
-                                    placeholder="Phone Number"
-                                    className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
-                                />
-                            </div>
-                            <div className="flex flex-col w-full md:w-1/2 pl-2">
-                                <input
                                     type="text"
                                     value={updatedUser.username}
                                     onChange={handleChange}
                                     name="username"
                                     placeholder="Create Username"
-                                    className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
-                                />
-                                <input
-                                    type="password"
-                                    value={updatedUser.password}
-                                    onChange={handleChange}
-                                    name="password"
-                                    placeholder="Create Password"
                                     className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
                                 />
                                 <input
@@ -130,17 +141,43 @@ const Profile = () => {
                                     placeholder="Confirm Password"
                                     className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
                                 />
+                            </div>
+                            <div className="flex flex-col w-full md:w-1/2 pl-2">
+                                <input
+                                    type="text"
+                                    value={updatedUser.lastName}
+                                    onChange={handleChange}
+                                    name="lastName"
+                                    placeholder="Last Name"
+                                    className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
+                                />
+                                <input
+                                    type="tel"
+                                    value={updatedUser.phone}
+                                    onChange={handleChange}
+                                    name="phone"
+                                    placeholder="Phone Number"
+                                    className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
+                                />
+                                <input
+                                    type="password"
+                                    value={updatedUser.password}
+                                    onChange={handleChange}
+                                    name="password"
+                                    placeholder="Create Password"
+                                    className="border-2 border-gray-300 rounded mb-3 p-2 focus:border-blue-500 focus:outline-none"
+                                />
                                 <div className="flex justify-between">
-                                    <button 
+                                    <button
                                         type="button"
-                                        className="mt-4 px-6 py-2 bg-green-500 text-white font-semibold rounded-lg shadow hover:bg-green-600 transition duration-300"
+                                        className="m-0.5 px-6 py-2 bg-green-500 text-white font-semibold rounded-lg shadow hover:bg-green-600 transition duration-300"
                                         onClick={handleSave}
                                     >
                                         Save
                                     </button>
-                                    <button 
+                                    <button
                                         type="button"
-                                        className="mt-4 px-6 py-2 bg-red-500 text-white font-semibold rounded-lg shadow hover:bg-red-600 transition duration-300"
+                                        className="m-0.5 px-6 py-2 bg-white text-red-500 font-semibold rounded-lg shadow hover:text-white hover:bg-red-500 transition duration-300"
                                         onClick={() => setEditMode(false)}
                                     >
                                         Cancel
