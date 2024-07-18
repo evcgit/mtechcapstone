69668cdf5275fa94db46ad8e45ae7846e3591e0d
Evan.Cortez <EVAN.CORTEZ3145@STU.MTEC.EDU>
2024-07-18 10:13:40 -0600

    background home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 import { useAuth } from '../auth/auth';
 import Card from '../components/Card';
 import Calendar from '../components/Calender';
+import backgroundImage from '../assets/homebg.webp';
 
 const Home = ({ first_name }) => {
   useAuth();
@@ -18,7 +19,10 @@ const Home = ({ first_name }) => {
   return (
     <div className='bg-gray-800 h-screen flex flex-col'>
       <Header />
-      <div className='w-screen mt-10 flex h-5/6'>
+      <div className='w-screen flex h-screen bg-cover'
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+      }}>
         <div className="w-1/4 ml-12 rounded h-5/6 p-4 bg-slate-100 overflow-hidden flex flex-col">
           <h2 className="text-2xl text-center font-semibold mb-2">{new Date().toLocaleDateString()}</h2>
           <Calendar />
