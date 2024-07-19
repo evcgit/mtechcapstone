1f9f73234d95245508839a729ba4588733697f73
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-07-19 10:08:48 -0600

    register page

--- client/src/pages/user/Register.js
+++ client/src/pages/user/Register.js
@@ -6,14 +6,11 @@ import backgroundImage from '../../assets/registerbg.webp';
 import LoadingSpinner from '../../components/LoadingSpinner';
 
 const Register = () => {
-    
     const [openCards, setOpenCards] = useState([]);
     const [cartItems, setCartItems] = useState([]);
     const [courses, setCourses] = useState([]);
     const [loading, setLoading] = useState(true);
 
-
-    
     useEffect(() => {
         const fetchCourses = async () => {
             try {
@@ -21,9 +18,9 @@ const Register = () => {
                 if (response.ok) {
                     const data = await response.json();
                     setTimeout(() => {
-                      setCourses(data);
-                      setLoading(false); 
-                    }, 700); 
+                        setCourses(data);
+                        setLoading(false);
+                    }, 700);
                 } else {
                     console.error('Failed to fetch courses');
                 }
@@ -44,50 +41,55 @@ const Register = () => {
     };
 
     const addToCart = (course) => {
-        setCartItems([...cartItems, course]);
+        if (!cartItems.some(item => item.string_id === course.string_id)) {
+            setCartItems([...cartItems, course]);
+            alert('Course added to cart');
+        } else {
+            alert('Course is already in the cart');
+        }
     };
 
     return (
         <div className='min-h-screen w-screen flex flex-col bg-cover'
-					style={{backgroundImage: `url(${backgroundImage})`}}>
+            style={{ backgroundImage: `url(${backgroundImage})` }}>
             {loading ? (
                 <LoadingSpinner />
             ) : (
-              <>
-            <Header />
-            <div className='flex flex-grow p-20'>
-                <div className="custom-scrollbar w-2/3 ml-10 overflow-y-auto p-6 bg-white/20 backdrop-blur-md rounded-3xl shadow-md" style={{maxHeight: '75vh', minHeight: '75vh' }}>
-                    <div className="flex flex-col space-y-8 p-1">
-                        {courses.map(course => (
-                            <RegisterCard
-                                key={course.string_id} 
-                                title={course.title}
-                                description={course.description}
-                                string_id={course.string_id}
-                                schedule={course.schedule}
-                                classroom_number={course.classroom_number}
-                                spots_left={course.maximum_capacity}
-                                credits={course.credit_hours}
-                                cost={`$${course.tuition_cost}`}
-                                isOpen={openCards.includes(course.string_id)}
-                                toggleCard={() => toggleCard(course.string_id)}
-                                addToCart={() => addToCart({
-                                    title: course.title,
-                                    string_id: course.string_id,
-                                    cost: `$${course.tuition_cost}`
-                                })}
-                            />
-                        ))}
+                <>
+                    <Header />
+                    <div className='flex flex-grow p-20'>
+                        <div className="custom-scrollbar w-2/3 ml-10 overflow-y-auto p-6 bg-white/20 backdrop-blur-md rounded-3xl shadow-md" style={{ maxHeight: '75vh', minHeight: '75vh' }}>
+                            <div className="flex flex-col space-y-8 p-1">
+                                {courses.map(course => (
+                                    <RegisterCard
+                                        key={course.string_id}
+                                        title={course.title}
+                                        description={course.description}
+                                        string_id={course.string_id}
+                                        schedule={course.schedule}
+                                        classroom_number={course.classroom_number}
+                                        spots_left={course.maximum_capacity}
+                                        credits={course.credit_hours}
+                                        cost={`$${course.tuition_cost}`}
+                                        isOpen={openCards.includes(course.string_id)}
+                                        toggleCard={() => toggleCard(course.string_id)}
+                                        addToCart={() => addToCart({
+                                            title: course.title,
+                                            string_id: course.string_id,
+                                            cost: `$${course.tuition_cost}`
+                                        })}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                        <div className="w-2/4 ml-20 flex justify-end">
+                            <div className="w-2/3">
+                                <ShoppingCart cartItems={cartItems} setCartItems={setCartItems} />
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="w-2/4 ml-20 flex justify-end">
-                    <div className="w-2/3">
-                        <ShoppingCart cartItems={cartItems} setCartItems={setCartItems} />
-                    </div>
-                </div>
-            </div>
-            </>
-          )}
+                </>
+            )}
         </div>
     );
 }
