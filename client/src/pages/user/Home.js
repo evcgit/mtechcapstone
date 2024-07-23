eeeead8c4490e910bc372aa1e9e0be0b08f1360d
Kobeanderson51 <79375386+Kobeanderson51@users.noreply.github.com>
2024-07-23 10:17:49 -0600

    courses

--- client/src/pages/user/Home.js
+++ client/src/pages/user/Home.js
@@ -3,10 +3,8 @@ import Header from '../../components/user/Header';
 import { useAuth } from '../../auth/auth';
 import Calendar from '../../components/user/Calender';
 import backgroundImage from '../../assets/homebg.webp';
-import LoadingSpinner  from '../../components/LoadingSpinner';
+import LoadingSpinner from '../../components/LoadingSpinner';
 import { useSnackbar } from 'notistack';
-
-
 
 const Home = () => {
   useAuth();
@@ -14,34 +12,88 @@ const Home = () => {
 
   const [firstName, setFirstName] = useState('');
   const [loading, setLoading] = useState(true);
+  const [registeredCourses, setRegisteredCourses] = useState([]);
 
   useEffect(() => {
     const fetchProfileInfo = async () => {
-      fetch('/user/profile', {
-        method: 'GET',
+      try {
+        const profileRes = await fetch('/user/profile', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        const profileData = await profileRes.json();
+        
+        if (profileData.error) {
+          enqueueSnackbar(profileData.error, { variant: 'error' });
+        } else {
+          setFirstName(profileData.first_name);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        enqueueSnackbar('Error fetching profile', { variant: 'error' });
+      }
+    };
+
+    const fetchRegisteredCourses = async () => {
+      try {
+        const coursesRes = await fetch('/courses/schedule', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          }
+        });
+        const coursesData = await coursesRes.json();
+
+        if (coursesData.error) {
+          enqueueSnackbar(coursesData.error, { variant: 'error' });
+        } else {
+          setRegisteredCourses(coursesData);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        enqueueSnackbar('Error fetching courses', { variant: 'error' });
+      }
+    };
+
+    const fetchData = async () => {
+      await fetchProfileInfo();
+      await fetchRegisteredCourses();
+      setLoading(false); 
+    };
+
+    fetchData();
+  }, [enqueueSnackbar]);
+
+  const handleRemoveCourse = async (courseId) => {
+    try {
+      const response = await fetch(`/courses/remove/${courseId}`, {
+        method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
           'Authorization': `Bearer ${localStorage.getItem('token')}`
         }
-      })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.error) {
-          enqueueSnackbar(data.error, { variant: 'error' });
-        } else {
-          setTimeout(() => {
-            setFirstName(data.first_name);
-            setLoading(false); 
-          }, 800); 
-        }
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-        enqueueSnackbar('Error', error, { variant: 'error' });
       });
-    };
-    fetchProfileInfo();
-  }, [enqueueSnackbar]);
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.error || 'Error removing course');
+      }
+
+      const data = await response.json();
+      enqueueSnackbar(data.message, { variant: 'success' });
+      
+      setRegisteredCourses((prevCourses) =>
+        prevCourses.filter(course => course.string_id !== courseId)
+      );
+    } catch (error) {
+      console.error('Error removing course:', error);
+      enqueueSnackbar(`Error removing course: ${error.message}`, { variant: 'error' });
+    }
+  };
 
   const today = new Date();
   const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -55,22 +107,38 @@ const Home = () => {
       ) : (
         <>
           <Header />
-          <div className='flex flex-col lg:flex-row lg:space-x-4 p-4 lg:p-8 h-full'>
-            <div className='flex-1 lg:w-1/4 bg-slate-100 rounded-lg p-4 h-full flex flex-col'>
+          <div className='flex flex-col lg:flex-row lg:space-x-8 p-4 lg:p-8 h-full'>
+            <div className='flex-1 lg:w-1/3 bg-slate-100 rounded-lg p-4 h-5/6 flex flex-col'> {/* Adjust width */}
               <h2 className='text-lg lg:text-xl text-center font-semibold mb-2'>{dayName}</h2>
               <h2 className='text-xl lg:text-2xl text-center font-semibold mb-2'>{today.toLocaleDateString()}</h2>
-              <div className='flex-1 overflow-y-auto'>
+              <div className='flex-1 overflow-auto'>
                 <Calendar />
               </div>
             </div>
-            <div className='flex-1 ml-4 mr-12 mt-4 flex flex-col space-y-4 max-xl:w-11/12 max-xl:ml-4'>
-              <div className='bg-white/20 backdrop-blur-md rounded-3xl shadow-md p-6 flex justify-center items-center'>
-                <p className='text-white text-2xl font-bold'>
+            <div className='flex-1 lg:w-2/3 flex flex-col space-y-4'> {/* Adjust width */}
+              <div className='bg-white/60 backdrop-blur-md rounded-3xl shadow-md p-6 flex justify-center items-center'>
+                <p className='text-gray-800 text-2xl font-bold'>
                   Welcome {firstName}!
                 </p>
               </div>
-              <div className='p-6 bg-white/20 backdrop-blur-md rounded-3xl shadow-md flex flex-col'>
-                {/* Your other components go here */}
+              <div className='p-6 bg-white/60 backdrop-blur-md rounded-3xl shadow-md flex flex-col h-96 custom-scrollbar'>
+                <h3 className='text-xl font-semibold mb-4'>Registered Courses</h3>
+                <ul className='overflow-y-auto space-y-2 custom-scrollbar'>
+                  {registeredCourses.length > 0 ? (
+                    registeredCourses.map(course => (
+                      <li key={course.string_id} className='flex items-center justify-between p-2 bg-white rounded-lg shadow-md'>
+                        <span>{course.title} - {course.schedule}</span>
+                        <button 
+                          onClick={() => handleRemoveCourse(course.string_id)}
+                          className='bg-red-500 text-white py-1 px-3 rounded hover:bg-red-600'>
+                          Remove
+                        </button>
+                      </li>
+                    ))
+                  ) : (
+                    <p>No registered courses.</p>
+                  )}
+                </ul>
               </div>
             </div>
           </div>
@@ -81,4 +149,3 @@ const Home = () => {
 };
 
 export default Home;
-
