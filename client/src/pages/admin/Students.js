f0cc768dfe7757c325c97397634879e962006c32
evcgit <evanvcortez@gmail.com>
2024-07-26 09:25:34 -0600

    authorization bug

--- client/src/pages/admin/Students.js
+++ client/src/pages/admin/Students.js
@@ -24,6 +24,7 @@ const Students = () => {
 					headers: {
             method: 'GET',
 						'Content-Type': 'Application/json',
+						'Authorization': `Bearer ${localStorage.getItem('token')}`
 					}
 				});
 				if (response.ok) {
