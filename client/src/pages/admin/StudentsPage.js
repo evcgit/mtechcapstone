4175922b0e42467762c097946db3fc5cc6ef70bf
evcgit <evanvcortez@gmail.com>
2024-07-27 12:55:53 -0600

    render students page bug2

--- client/src/pages/admin/StudentsPage.js
+++ client/src/pages/admin/StudentsPage.js
@@ -1,54 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import AdminHeader from '../../components/admin/AdminHeader';
 import { useAdminAuth } from '../../auth/auth';
 import backgroundImage from '../../assets/registerbg.webp';
-import { useSnackbar } from 'notistack';
 
 
 const Students = () => {
 	useAdminAuth();
-	const { enqueueSnackbar } = useSnackbar();
-	const [loading, setLoading] = useState(true);
-	const [students, setStudents] = useState([]);
-	const [openCards, setOpenCards] = useState([]);
-	const [openEdit, setOpenEdit] = useState(false);
-	const [selectedStudent, setSelectedStudent] = useState(null);
-
-	useEffect(() => {
-		const fetchStudents = async () => {
-			const token = localStorage.getItem('token');
-			if (!token) {
-				enqueueSnackbar('No token found. Please log in again.', { variant: 'error' });
-				setLoading(false);
-				return;
-			}
-
-			try {
-				const response = await fetch('/students', {
-					headers: {
-						'Content-Type': 'application/json',
-						'Authorization': `Bearer ${token}`
-					}
-				});
-
-				const data = await response.json();
-
-				if (response.ok) {
-					setStudents(data);
-				} else {
-					enqueueSnackbar(data.errorMessage || 'Error fetching students', { variant: 'error' });
-				}
-
-				setLoading(false);
-			} catch (error) {
-				console.error('Error fetching students:', error);
-				enqueueSnackbar('Error fetching students', { variant: 'error' });
-				setLoading(false);
-			}
-		};
-
-		fetchStudents();
-	}, [enqueueSnackbar]);
 
 
 	return (
